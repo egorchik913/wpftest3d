56b11825b3878c2078e6bf304d80b1eeda0abba7
egork <egork@LAPTOP-0O02AG3K>
2023-07-24 13:02:08 +0300

    о

--- wpftest3d/MainWindow.xaml.cs
+++ wpftest3d/MainWindow.xaml.cs
@@ -1,26 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using HelixToolkit.Wpf;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using HelixToolkit.Wpf;
-using System.Windows.Media.Media3D;
-using System.Text.RegularExpressions;
-using System.Reflection;
-using System.Security.Policy;
-using System.Threading;
-using System.Runtime.Remoting.Channels;
-using System.Runtime.Remoting.Messaging;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Media3D;
 
 namespace wpftest3d
 {
@@ -28,12 +14,16 @@ namespace wpftest3d
     public partial class MainWindow : Window
     {
         Model3D pin;
+
         Model3D model1;
         Model3D model1Dark;
+
         Model3D model2;
         Model3D model2Dark;
+
         Model3D model3;
         Model3D model3Dark;
+
         Model3D model4;
         Model3D model5;
         Model3D model6;
@@ -68,57 +58,111 @@ namespace wpftest3d
             animation.AutoReverse = true;
             animation.Duration = TimeSpan.FromSeconds(1);
             animation.RepeatBehavior = RepeatBehavior.Forever;
+
+            //Функции
+            void UI_MouseEnter(ModelUIElement3D modelUIElement, ModelUIElement3D modelUIElementDark)
+            {
+                viewport.Children.Remove(modelUIElement);
+                viewport.Children.Add(modelUIElementDark);
+            };
+
+            void UI_MouseLeave(ModelUIElement3D modelUIElement, ModelUIElement3D modelUIElementDark)
+            {
+                viewport.Children.Remove(modelUIElementDark);
+                viewport.Children.Add(modelUIElement);
+            };
+
+            void UI_MouseDown(ModelUIElement3D modelUIElement, ModelUIElement3D modelUIElementDark)
+            {
+                
+            };
+
+            bool[] isFunctionEnabled = { true, true, true, true, true, true, true, true, true, true };
+
+
             //Альметьевск
             model1 = importer.Load("D:/3D/TATNEFT/MapOtdelno/almet.obj");
             ModelUIElement3D UImodel1 = new ModelUIElement3D();
             UImodel1.Model = model1;
             viewport.Children.Add(UImodel1);
-
             //АльметьевскDark
             model1Dark = importer.Load("D:/3D/TATNEFT/MapOtdelnoDark/almetDark.obj");
             ModelUIElement3D UImodel1Dark = new ModelUIElement3D();
             UImodel1Dark.Model = model1Dark;
+            UImodel1Dark.Transform = new TranslateTransform3D(0, 0.01, 0);
 
             UImodel1.MouseEnter += (sender, e) =>
             {
-                viewport.Children.Remove(UImodel1);
-                viewport.Children.Add(UImodel1Dark);
-                UImodel1Dark.Transform = new TranslateTransform3D(0, 0.01, 0);
+                UI_MouseEnter(UImodel1, UImodel1Dark);
+                NameNGDY.Text = "НГДУ АльметНефть";
             };
 
             UImodel1Dark.MouseLeave += (sender, e) =>
             {
-                viewport.Children.Remove(UImodel1Dark);
-                viewport.Children.Add(UImodel1);
+                if (isFunctionEnabled[0] == true)
+                {
+                    UI_MouseLeave(UImodel1, UImodel1Dark);
+                    NameNGDY.Text = null;
+                }
             };
 
-           
+            UImodel1Dark.MouseDown += (sender, e) =>
+            {
+                switch (isFunctionEnabled[0])
+                {
+                    case true:
+                        isFunctionEnabled[0] = !isFunctionEnabled[0];
+                        //if (NameNGDY.Text != null)
+                        //{
+                        //    NameNGDY.Text += ", НГДУ АльметНефть";
+                        //}
+                        break;
+                    case false:
+                        isFunctionEnabled[0] = !isFunctionEnabled[0];
+                        break;
+                }
+            };
+
             //Азнакаево
             model2 = importer.Load("D:/3D/TATNEFT/MapOtdelno/aznakaev.obj");
             ModelUIElement3D UImodel2 = new ModelUIElement3D();
             UImodel2.Model = model2;
             viewport.Children.Add(UImodel2);
-
             //АзнакаевоDark
             model2Dark = importer.Load("D:/3D/TATNEFT/MapOtdelnoDark/aznakaevDark.obj");
             ModelUIElement3D UImodel2Dark = new ModelUIElement3D();
             UImodel2Dark.Model = model2Dark;
+            UImodel2Dark.Transform = new TranslateTransform3D(0, 0.01, 0);
 
             UImodel2.MouseEnter += (sender, e) =>
             {
-                viewport.Children.Remove(UImodel2);
-                viewport.Children.Add(UImodel2Dark);
-                UImodel2Dark.Transform = new TranslateTransform3D(0, 0.01, 0);
-
+                UI_MouseEnter(UImodel2, UImodel2Dark);
                 viewport.Children.Add(UIpin);
                 UIpin.Transform.BeginAnimation(TranslateTransform3D.OffsetYProperty, animation);
+                NameNGDY.Text = "НГДУ АзнакаевНефть";
             };
 
             UImodel2Dark.MouseLeave += (sender, e) =>
             {
-                viewport.Children.Remove(UIpin);
-                viewport.Children.Remove(UImodel2Dark);
-                viewport.Children.Add(UImodel2);
+                if (isFunctionEnabled[1] == true)
+                {
+                    UI_MouseLeave(UImodel2, UImodel2Dark);
+                    viewport.Children.Remove(UIpin);
+                    NameNGDY.Text = null;
+                }
+            };
+
+            UImodel2Dark.MouseDown += (sender, e) =>
+            {
+                switch (isFunctionEnabled[1])
+                {
+                    case true:
+                        isFunctionEnabled[1] = !isFunctionEnabled[1];
+                        break;
+                    case false:
+                        isFunctionEnabled[1] = !isFunctionEnabled[1];
+                        break;
+                }
             };
 
             //Бавлы
@@ -126,23 +170,39 @@ namespace wpftest3d
             ModelUIElement3D UImodel3 = new ModelUIElement3D();
             UImodel3.Model = model3;
             viewport.Children.Add(UImodel3);
-
             //БавлыDark
             model3Dark = importer.Load("D:/3D/TATNEFT/MapOtdelnoDark/bavlinskDark.obj");
             ModelUIElement3D UImodel3Dark = new ModelUIElement3D();
             UImodel3Dark.Model = model3Dark;
+            UImodel3Dark.Transform = new TranslateTransform3D(0, 0.01, 0);
 
             UImodel3.MouseEnter += (sender, e) =>
             {
-                viewport.Children.Remove(UImodel3);
-                viewport.Children.Add(UImodel3Dark);
-                UImodel3Dark.Transform = new TranslateTransform3D(0, 0.01, 0);
+                UI_MouseEnter(UImodel3, UImodel3Dark);
+                UIpin.Transform.BeginAnimation(TranslateTransform3D.OffsetYProperty, animation);
+                NameNGDY.Text = "НГДУ БавлыНефть";
             };
 
             UImodel3Dark.MouseLeave += (sender, e) =>
             {
-                viewport.Children.Remove(UImodel3Dark);
-                viewport.Children.Add(UImodel3);
+                if (isFunctionEnabled[2] == true)
+                {
+                    UI_MouseLeave(UImodel3, UImodel3Dark);
+                    NameNGDY.Text = null;
+                }
+            };
+
+            UImodel3Dark.MouseDown += (sender, e) =>
+            {
+                switch (isFunctionEnabled[2])
+                {
+                    case true:
+                        isFunctionEnabled[2] = !isFunctionEnabled[2];
+                        break;
+                    case false:
+                        isFunctionEnabled[2] = !isFunctionEnabled[2];
+                        break;
+                }
             };
 
             //Джалиль
@@ -150,32 +210,12 @@ namespace wpftest3d
             ModelUIElement3D UImodel4 = new ModelUIElement3D();
             UImodel4.Model = model4;
             viewport.Children.Add(UImodel4);
-            UImodel4.MouseEnter += (sender, e) =>
-            {
-                var TransModel = model4;
-                TransModel.Transform = new TranslateTransform3D(0, 0, -0.05);
-            };
-            UImodel4.MouseLeave += (sender, e) =>
-            {
-                var TransModel = model4;
-                TransModel.Transform = new TranslateTransform3D(0, 0, 0);
-            };
 
             //Елхов
             model5 = importer.Load("D:/3D/TATNEFT/MapOtdelno/elhov.obj");
             ModelUIElement3D UImodel5 = new ModelUIElement3D();
             UImodel5.Model = model5;
             viewport.Children.Add(UImodel5);
-            UImodel5.MouseEnter += (sender, e) =>
-            {
-                var TransModel = model5;
-                TransModel.Transform = new TranslateTransform3D(0, 0, -0.05);
-            };
-            UImodel5.MouseLeave += (sender, e) =>
-            {
-                var TransModel = model5;
-                TransModel.Transform = new TranslateTransform3D(0, 0, 0);
-            };
 
             //Призрак
             model6 = importer.Load("D:/3D/TATNEFT/MapOtdelno/ghost.obj");
@@ -188,64 +228,24 @@ namespace wpftest3d
             ModelUIElement3D UImodel7 = new ModelUIElement3D();
             UImodel7.Model = model7;
             viewport.Children.Add(UImodel7);
-            UImodel7.MouseEnter += (sender, e) =>
-            {
-                var TransModel = model7;
-                TransModel.Transform = new TranslateTransform3D(0, 0, -0.05);
-            };
-            UImodel7.MouseLeave += (sender, e) =>
-            {
-                var TransModel = model7;
-                TransModel.Transform = new TranslateTransform3D(0, 0, 0);
-            };
 
             //Нурлат
             model8 = importer.Load("D:/3D/TATNEFT/MapOtdelno/nurlat.obj");
             ModelUIElement3D UImodel8 = new ModelUIElement3D();
             UImodel8.Model = model8;
             viewport.Children.Add(UImodel8);
-            UImodel8.MouseEnter += (sender, e) =>
-            {
-                var TransModel = model8;
-                TransModel.Transform = new TranslateTransform3D(0, 0, -0.05);
-            };
-            UImodel8.MouseLeave += (sender, e) =>
-            {
-                var TransModel = model8;
-                TransModel.Transform = new TranslateTransform3D(0, 0, 0);
-            };
 
             //Прикам
             model9 = importer.Load("D:/3D/TATNEFT/MapOtdelno/prikam.obj");
             ModelUIElement3D UImodel9 = new ModelUIElement3D();
             UImodel9.Model = model9;
             viewport.Children.Add(UImodel9);
-            UImodel9.MouseEnter += (sender, e) =>
-            {
-                var TransModel = model9;
-                TransModel.Transform = new TranslateTransform3D(0, 0, -0.05);
-            };
-            UImodel9.MouseLeave += (sender, e) =>
-            {
-                var TransModel = model9;
-                TransModel.Transform = new TranslateTransform3D(0, 0, 0);
-            };
 
             //Ямаш
             model10 = importer.Load("D:/3D/TATNEFT/MapOtdelno/yamash.obj");
             ModelUIElement3D UImodel10 = new ModelUIElement3D();
             UImodel10.Model = model10;
             viewport.Children.Add(UImodel10);
-            UImodel10.MouseEnter += (sender, e) =>
-            {
-                var TransModel = model10;
-                TransModel.Transform = new TranslateTransform3D(0, 0, -0.05);
-            };
-            UImodel10.MouseLeave += (sender, e) =>
-            {
-                var TransModel = model10;
-                TransModel.Transform = new TranslateTransform3D(0, 0, 0);
-            };
         }
     }
 }
